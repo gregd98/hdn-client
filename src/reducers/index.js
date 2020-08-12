85c02b13ffd7e0cd90ed1493f0e7ab81a515312e
Greg <gergodemeny@gmail.com>
2020-08-12 12:24:00 +0300

    Commit fix.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import staffReducer from './staffRedurer';
 import teamsReducer from './teamsReducer';
+import eventReducer from './eventReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   staff: staffReducer,
   teams: teamsReducer,
+  event: eventReducer,
 });
 
 export default rootReducer;
